2da8588ca2e7e42d9b4cd5d223bdd7eb536866f3
nguyentt <nguyentt.dev@gmail.com>
2018-04-08 23:55:32 +0200

    add ship and screen classes

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,19 +1,25 @@
 import sys, pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
 
 def run_game():
     # Initialize game and create a screen object
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+
+    screen = pygame.display.set_mode((ai_settings.screen_width , ai_settings.screen_height))
+
     pygame.display.set_caption("Alien Shooter")
+
+    # Make a ship
+    ship = Ship(screen)
 
     # Start the main loop for the game
     while True:
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
-        # Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
-        
+
 run_game()
---
+++ game_functions.py
@@ -0,0 +1,18 @@
+import sys, pygame
+
+
+def check_events():
+    # Watch for keyboard and mouse events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+
+def update_screen(ai_settings, screen, ship):
+    """Update images on the screen and flip to the new screen"""
+    # Redraw the screen during each pass through the loop
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
